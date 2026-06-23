3bb261f74e48d1b8063a51f2ea64e7588743076a
linhdc140594-create <linhdc140594@gmail.com>
2026-06-23 11:26:43 +0700

    Update StudentCards.tsx

--- src/pages/StudentCards.tsx
+++ src/pages/StudentCards.tsx
@@ -49,7 +49,7 @@ function StudentCard({ student, centerName, classInfo, theme }) {
           </div>
           <div style={{ 
             color: '#fff', 
-            fontSize: '20px', // TĂNG SIZE: từ 12px -> 14px
+            fontSize: '17px', // TĂNG SIZE: từ 12px -> 14px
             fontWeight: 900, lineHeight: 1.2, maxWidth: '45mm' 
           }}>
             {centerName || 'TRUNG TÂM'}
