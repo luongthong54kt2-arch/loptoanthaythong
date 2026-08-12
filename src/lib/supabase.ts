81ede18e4b1eab473fbd9ffc9d3003626afca31e
linhdc140594-create <linhdc140594@gmail.com>
2026-08-12 13:12:20 +0700

    fix: hardcode exact supabase url and key in supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = (import.meta.env.VITE_SUPABASE_URL as string) || 'https://jpfnuqanqbonienosxdp.supabase.co'
-const supabaseKey = (import.meta.env.VITE_SUPABASE_ANON_KEY as string) || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwZm51cWFucWJvbmllbm9zeGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY0OTgzNDgsImV4cCI6MjEwMjA3NDM0OH0.uq_rwjepwJC3QmTZSIL7XpI4HAFbWMilmaRtYWqlics'
+const supabaseUrl = 'https://jpfnuqanqbonienosxdp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwZm51cWFucWJvbmllbm9zeGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY0OTgzNDgsImV4cCI6MjEwMjA3NDM0OH0.uq_rwjepwJC3QmTZSIL7XpI4HAFbWMilmaRtYWqlics'
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
